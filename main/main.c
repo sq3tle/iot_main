b9b132c4199e09ba5ae5cc85dc5c7ed3949ba694
sq3tle <sq3tle@gmail.com>
2023-01-26 16:43:34 +0100

    final

--- main/main.c
+++ main/main.c
@@ -14,7 +14,7 @@ static void init_nvs(void)
 {
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NOT_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
+        ESP_ERROR_CHECK(nvs_flash_erase()); 
         ESP_ERROR_CHECK(nvs_flash_init());
     }
 }
